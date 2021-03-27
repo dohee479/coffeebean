351228c126af0c1a7544104c85b88e35bf031159
comdoller <nearermg@gmail.com>
2021-03-27 20:46:59 +0900

    Add BasketItems sql query in basketitems.xml
    1. Basketitem DTO, Dao, Service, mapper 작성
    2. products 테이블에 이미지 정보에 대한 열 속성이 누락되어 있음
    products_product_imgonmae,
    products_product_imgsnmae,
    products_product_imgtype
    로 poducts 테이블에 속성 추가 필요함
    DTO에 필드 이름을 위의 이름으로 작성함
    3. 찜하기에서 상품옵션 속성이 반드시 필요함
    상품상세페이지 -> 찜하기 -> 장바구니 -> 구매의 시나리오 이기 때문
    찜 테이블에서 상품 옵션 속성 추가 필요함.

--- src/main/java/com/mycompany/webapp/dto/Order.java
+++ src/main/java/com/mycompany/webapp/dto/Order.java
@@ -1,5 +1,75 @@
 package com.mycompany.webapp.dto;
 
-public class Order {
+import java.util.Date;
 
+public class Order {
+	
+	private int order_id;
+	private String order_receiver;
+	private String users_user_id;
+	private String order_tel;
+	private String order_address;
+	private String order_detail_address;
+	private Date order_date;
+	private String order_msg;
+	private int order_state; 
+	/*주문시작 : 1 배송중 : 2 배송완료 : 3 구매확정 : 4 취소/환불 :5*/
+	
+	public int getOrder_id() {
+		return order_id;
+	}
+	public void setOrder_id(int order_id) {
+		this.order_id = order_id;
+	}
+	public String getOrder_receiver() {
+		return order_receiver;
+	}
+	public void setOrder_receiver(String order_receiver) {
+		this.order_receiver = order_receiver;
+	}
+	public String getUsers_user_id() {
+		return users_user_id;
+	}
+	public void setUsers_user_id(String users_user_id) {
+		this.users_user_id = users_user_id;
+	}
+	public String getOrder_tel() {
+		return order_tel;
+	}
+	public void setOrder_tel(String order_tel) {
+		this.order_tel = order_tel;
+	}
+	public String getOrder_address() {
+		return order_address;
+	}
+	public void setOrder_address(String order_address) {
+		this.order_address = order_address;
+	}
+	public String getOrder_detail_address() {
+		return order_detail_address;
+	}
+	public void setOrder_detail_address(String order_detail_address) {
+		this.order_detail_address = order_detail_address;
+	}
+	public Date getOrder_date() {
+		return order_date;
+	}
+	public void setOrder_date(Date order_date) {
+		this.order_date = order_date;
+	}
+	public String getOrder_msg() {
+		return order_msg;
+	}
+	public void setOrder_msg(String order_msg) {
+		this.order_msg = order_msg;
+	}
+	public int getOrder_state() {
+		return order_state;
+	}
+	public void setOrder_state(int order_state) {
+		this.order_state = order_state;
+	}
+	
+	
+	
 }
