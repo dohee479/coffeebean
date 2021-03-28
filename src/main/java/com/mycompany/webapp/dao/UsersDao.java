9d4b5da73575e1112b7f1c41f16f8a31345db984
JOUNHO <dnsgh9177@naver.com>
2021-03-29 04:02:18 +0900

    Add Users sql

--- src/main/java/com/mycompany/webapp/dao/UsersDao.java
+++ src/main/java/com/mycompany/webapp/dao/UsersDao.java
@@ -8,8 +8,10 @@ import com.mycompany.webapp.dto.User;
 public interface UsersDao {
 
 	public int insert(User user);
-	public User selectByUid(String user_id);
+	public User selectById(String user_id);
+	public User selectByEmail(String user_id);
+	public int updateInfo(User user);
 	public int updatePassword(User user);
-	public int deleteUser(User user);
+	public int delete(String user_id);
 	
 }
--- src/main/java/com/mycompany/webapp/service/UsersService.java
+++ src/main/java/com/mycompany/webapp/service/UsersService.java
@@ -1,6 +1,7 @@
 package com.mycompany.webapp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.mycompany.webapp.dao.UsersDao;
@@ -17,17 +18,45 @@ public class UsersService {
 	}
 	
 	public String login(User user) {
-		User dbUser = usersDao.selectByUid(user.getUser_id());
+		User dbUser = usersDao.selectById(user.getUser_id());
 		
-		return "#";
+		if(dbUser==null) {
+			return "wrongUid";
+		}else {
+			BCryptPasswordEncoder bpe=new BCryptPasswordEncoder();
+			boolean result=bpe.matches(user.getUser_password(), dbUser.getUser_password());
+			if(result==false) {
+				return "wrongUpassword";
+			}
+		}
+		
+		
+		return "success";
 	}
 	
+	public User findId(User user) {
+		User dbUser=usersDao.selectByEmail(user.getUser_email());
+		return dbUser;
+		
+
+	}
+	
+	public User findPassword(User user) {
+		User dbUser=usersDao.selectById(user.getUser_id());
+		return dbUser;
+	}
+	
+	public void updateInfo(User user) {
+		usersDao.updateInfo(user);
+	}
+
 	public void updatePassword(User user) {
 		usersDao.updatePassword(user);
 	}
-	
-	public void deleteUser(User user) {
-		usersDao.deleteUser(user);
+
+	public void delete(String user_id) {
+		//컨트롤러에서 principal로 현재 로그인한 id 받아옴
+		usersDao.delete(user_id);
 	}
 	
 }
