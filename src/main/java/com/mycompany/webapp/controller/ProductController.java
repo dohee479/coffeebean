4fff41469d23753dd9dee3e7a7b1957872d9e506
dohee479 <dohee3959@gmail.com>
2021-04-02 16:59:26 +0900

    U_review

--- src/main/java/com/mycompany/webapp/controller/ProductController.java
+++ src/main/java/com/mycompany/webapp/controller/ProductController.java
@@ -66,15 +66,6 @@ public class ProductController {
 		return "/product/flavor";
 	}
 	
-	@GetMapping("/detail")
-	public String Detail(Model model) {
-		//현재 상품의 id를 받아와서 Service에 전달
-		List<Question> list=
-				questionsService.getListByProductQuestion(500);
-		model.addAttribute("list",list);
-		return "/product/detail";
-	}
-	
 	/* 상세페이지-상품 QnA 코드 */
 	@PostMapping("/detail-qna-create")
 	public String DetailCreateQna(Question question,Principal principal){
